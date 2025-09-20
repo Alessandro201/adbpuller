31461a7144c1b79aca6dec9954b400e057392551
Alessandro201 <poletti.alessandro0@gmail.com>
2025-09-20 13:09:49 +0200

    Added --include and --exclude options to filter the files to move.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@ use indicatif::{ProgressBar, ProgressStyle};
 use std::collections::HashSet;
 use std::fs::{read_to_string, OpenOptions};
 use std::io::Write;
+use std::iter::Zip;
 use std::path::{Path, PathBuf};
 use std::process::exit;
 use std::time::Duration;
 
+use std::vec::IntoIter;
 use std::{env, process};
 use unix_path::{Path as UnixPath, PathBuf as UnixPathBuf};
 
+use regex::Regex;
 use which::which;
 
 use clap::{ArgAction, Args, Parser};
@@ -40,10 +43,9 @@ struct Sources {
 /// Pull files from android using ADB drivers
 #[derive(Parser, Debug)]
 #[command(version, about)]
-#[command(long_about = "Pull files from android using ADB drivers
 
-Example:
-    ./adb_puller.exe -s /sdcard/DCIM")]
+// To add examples
+// #[command(long_about = "Pull files from android using ADB drivers")]
 struct Cli {
     #[command(flatten)]
     source: Sources,
@@ -56,8 +58,16 @@ struct Cli {
     #[arg(long, value_parser, num_args = 0..)]
     skip: Option<Vec<PathBuf>>,
 
+    /// Exclude files based on regex matching
+    #[arg(short='E',long, value_parser, num_args = 0..)]
+    exclude: Option<Vec<String>>,
+
+    /// Include files based on regex matching. Executes before exclude
+    #[arg(short='I',long, value_parser, num_args = 0..)]
+    include: Option<Vec<String>>,
+
     /// Print which files would be copied and where
-    #[arg(long, action = ArgAction::SetTrue)]
+    #[arg(short='n', long, action = ArgAction::SetTrue)]
     dry_run: bool,
 
     /// Overwrite files already present in the destination folder.
@@ -99,38 +109,39 @@ impl Cli {
     }
 }
 
-fn get_files_from_adb(adb_path: &PathBuf, root_path: &UnixPathBuf) -> Vec<UnixPathBuf> {
+fn get_files_from_adb(adb_path: &PathBuf, root_path: &UnixPathBuf) -> Result<Vec<UnixPathBuf>> {
     let mut file_list: Vec<UnixPathBuf> = Vec::new();
 
-    let mut cmd = process::Command::new(adb_path);
-    cmd.arg("shell");
-    cmd.arg("ls");
-    cmd.arg("-R");
-    cmd.arg(format!("\"{}\"", root_path.as_unix_str().to_str().unwrap()));
+    let output = process::Command::new(adb_path)
+        .arg("shell")
+        .arg("ls")
+        .arg("-R")
+        .arg(format!("\"{}\"", root_path.as_unix_str().to_str().unwrap()))
+        .output()
+        .context("Failed to execute the command")?
+        .stdout;
 
-    // println!("Running {:#?}", cmd);
-
-    let output = cmd.output().expect("Failed to execute the command").stdout;
-
-    let mut lines: Vec<String> = Vec::new();
-    match String::from_utf8(output) {
-        Ok(s) => lines.extend(s.lines().map(|x| x.trim().to_string())),
-        Err(err) => {
-            println!("Unable to read the output of `adb shell ls -R <path>`: {:#?}", err);
-            return file_list;
-        }
-    }
-
-    lines.retain(|x| !x.is_empty());
+    let lines: Vec<String> = String::from_utf8(output)
+        .context("Unable to read the output of `adb shell ls -R <path>`")?
+        .lines()
+        .map(|l| l.trim().to_string())
+        .filter(|x| !x.is_empty())
+        .collect();
 
     if lines.len() == 1 {
         file_list.push(UnixPathBuf::from(&lines[0]))
     }
 
+    // I DON'T UNDERSTAND WHAT THIS DOES. I NEED TO CHECK, but there seems to be erros. Ex, that
+    // happend if lines.len is 1? in the lines above it gets added to file_list, but then it gets
+    // added again in the following loop(?)
+    //
+    // If I'm not wrong, this code should make the paths from adb absolute, by joining the current
+    // root folder to the path
     let mut current_folder_root: UnixPathBuf = UnixPathBuf::from(&root_path); // default, but should be changed right away
     for line in lines.into_iter() {
         if line.starts_with('/') {
-            current_folder_root = UnixPathBuf::from(&line[..line.len() - 1]);
+            current_folder_root = UnixPathBuf::from(&line.trim_end_matches(['/', ':']));
             if let Some(i) = file_list.iter().position(|x| x == &current_folder_root) {
                 file_list.remove(i);
             }
@@ -140,7 +151,7 @@ fn get_files_from_adb(adb_path: &PathBuf, root_path: &UnixPathBuf) -> Vec<UnixPa
         }
     }
 
-    file_list
+    Ok(file_list)
 }
 
 struct SrcDestFiles {
@@ -157,7 +168,7 @@ impl SrcDestFiles {
     }
 
     /// Moves all the elements of `other` into `self`, leaving `other` empty.
-    fn append(&mut self, other: &mut SrcDestFiles) {
+    fn extend_from(&mut self, mut other: SrcDestFiles) {
         self.src_files.append(&mut other.src_files);
         self.dest_files.append(&mut other.dest_files);
     }
@@ -169,46 +180,21 @@ impl SrcDestFiles {
     fn len(&self) -> usize {
         self.src_files.len()
     }
-}
 
-impl IntoIterator for SrcDestFiles {
-    type Item = (UnixPathBuf, BasePathBuf);
-    type IntoIter = SrcDestFilesIntoIterator;
-
-    fn into_iter(self) -> Self::IntoIter {
-        SrcDestFilesIntoIterator { files: self, index: 0 }
+    fn into_iter(self) -> Zip<IntoIter<UnixPathBuf>, IntoIter<BasePathBuf>> {
+        self.src_files.into_iter().zip(self.dest_files.into_iter()).into_iter()
     }
 }
 
-struct SrcDestFilesIntoIterator {
-    files: SrcDestFiles,
-    index: usize,
-}
-
-impl Iterator for SrcDestFilesIntoIterator {
-    type Item = (UnixPathBuf, BasePathBuf);
-
-    fn next(&mut self) -> Option<(UnixPathBuf, BasePathBuf)> {
-        let src_path = match self.files.src_files.get(self.index) {
-            Some(path) => path.clone(),
-            None => return None,
-        };
-        let dest_path = match self.files.dest_files.get(self.index) {
-            Some(path) => path.clone(),
-            None => return None,
-        };
-        self.index += 1;
-        Some((src_path, dest_path))
-    }
-}
-
-fn get_files_to_skip(skip: &Option<Vec<PathBuf>>) -> HashSet<String> {
+fn get_files_to_skip(files_with_paths_to_skip: &Option<Vec<PathBuf>>) -> HashSet<String> {
     let mut hs: HashSet<String> = HashSet::new();
-    if let Some(skip_inside) = skip {
-        for path in skip_inside {
-            for line in read_to_string(path).unwrap_or_default().lines().map(String::from) {
-                hs.insert(line);
-            }
+    if files_with_paths_to_skip.is_none() {
+        return hs;
+    }
+    let files_with_paths_to_skip = files_with_paths_to_skip.as_ref().unwrap();
+    for path in files_with_paths_to_skip {
+        for line in read_to_string(path).unwrap_or_default().lines().map(String::from) {
+            hs.insert(line);
         }
     }
     hs
@@ -220,7 +206,7 @@ fn connected_to_adb_server(adb_path: &PathBuf, retries: Option<usize>) -> bool {
     let output = match process::Command::new(adb_path).arg("devices").stdout(process::Stdio::piped()).output() {
         Ok(output) => output,
         Err(_) => {
-            println!(
+            eprintln!(
                 "Unable to check if adb is connected. \nADB path: \"{}\"",
                 adb_path.as_path().to_str().unwrap()
             );
@@ -264,52 +250,72 @@ fn get_adb_path() -> Result<PathBuf> {
     }
 }
 
-fn build_file_list(adb_path: &PathBuf, args: &Cli) -> SrcDestFiles {
-    let files_to_skip = get_files_to_skip(&args.skip);
+fn build_file_list(adb_path: &PathBuf, args: &Cli) -> Result<SrcDestFiles> {
+    let to_skip = get_files_to_skip(&args.skip);
+    let regex_to_skip: Vec<Regex> = args
+        .exclude
+        .clone()
+        .unwrap_or_default()
+        .iter()
+        .map(|pattern| Regex::new(pattern).unwrap())
+        .collect();
+    let regex_to_keep: Vec<Regex> = args
+        .include
+        .clone()
+        .unwrap_or_default()
+        .iter()
+        .map(|pattern| Regex::new(pattern).unwrap())
+        .collect();
     let mut files = SrcDestFiles::new();
 
-    for root_src in args.source.sources.iter() {
-        let mut file_list = get_files_from_adb(adb_path, root_src);
-        println!("{:7} files found in {:?}", file_list.len(), &root_src);
-        file_list.retain(|x| !files_to_skip.contains(x.to_str().unwrap()));
+    for src in args.source.sources.iter() {
+        let mut files_in_src = get_files_from_adb(adb_path, src)?;
+        eprintln!("{:7} files found in {:?}", files_in_src.len(), &src);
+        files_in_src.retain(|x| !to_skip.contains(x.to_str().unwrap()));
+        files_in_src.retain(|x| {
+            let file = x.to_str().unwrap();
+            regex_to_keep.iter().any(|pattern| pattern.is_match(file))
+        });
+        files_in_src.retain(|x| {
+            let file = x.to_str().unwrap();
+            !(regex_to_skip.iter().any(|pattern| pattern.is_match(file)))
+        });
 
-        let mut temp_files = build_destination_files(&file_list, args.dest.as_path(), root_src, args.force);
-        println!("{:7} to copy", temp_files.len());
+        let temp_files = build_destination_files(&files_in_src, args.dest.as_path(), src, args.force)?;
+        eprintln!("{:7} to copy", temp_files.len());
 
-        files.append(&mut temp_files)
+        files.extend_from(temp_files)
     }
-    files
+    Ok(files)
 }
 
-fn build_destination_files(file_list: &[UnixPathBuf], root_dest: &Path, root_src: &UnixPathBuf, force: bool) -> SrcDestFiles {
+fn build_destination_files(file_list: &[UnixPathBuf], root_dest: &Path, root_src: &UnixPathBuf, force: bool) -> Result<SrcDestFiles> {
     let mut files = SrcDestFiles::new();
 
     for file in file_list.iter() {
         let file_rel_to_src: &UnixPath = match file.strip_prefix(root_src.parent().unwrap()) {
             Ok(path) => path,
             Err(_) => {
-                println!(
-                    "Unable to strip the prefix {:?} from {:?} when tying to find its corresponding destination",
+                eprintln!(
+                    "Unable to strip the prefix {:?} from {:?} when trying to find its corresponding destination. Continuing..",
                     &root_src, &file
                 );
                 continue;
             }
         };
 
-        let dest = root_dest.join(file_rel_to_src.as_unix_str().to_str().unwrap());
-
+        let dest = root_dest.join(file_rel_to_src.to_str().unwrap());
         if dest.exists() && !force {
             continue;
         }
-
         files.src_files.push(file.to_owned());
         files.dest_files.push(BasePathBuf::new(dest).unwrap());
     }
 
-    files
+    Ok(files)
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args: Cli = {
         // Limit scope to remove mutability
         let mut args = Cli::parse();
@@ -319,7 +325,7 @@ fn main() {
 
     let adb_path = match get_adb_path() {
         Ok(path) => {
-            println!("Using adb from: {path:?}");
+            eprintln!("Using adb from: {path:?}");
             path
         }
         Err(err) => {
@@ -328,21 +334,20 @@ fn main() {
         }
     };
 
-    println!("Checking if a device is attached to adb server..");
+    eprintln!("Checking if a device is attached to adb server..");
     if !connected_to_adb_server(&adb_path, None) {
-        println!("No device found. Try executing \"{} devices\"", adb_path.as_path().to_str().unwrap());
+        eprintln!("No device found. Try executing \"{} devices\"", adb_path.as_path().to_str().unwrap());
         exit(1);
     }
 
-    println!("Building file list, it may take some time...");
+    eprintln!("Building file list, it may take some time...");
 
-    let files = build_file_list(&adb_path, &args);
+    let files = build_file_list(&adb_path, &args)?;
 
     if args.source.sources.len() > 1 {
-        println!("\n{} total files to copy", files.dest_files.len());
+        eprintln!("\n{} total files to copy", files.dest_files.len());
     }
 
-    // Print files to copy if --dry-run
     if args.dry_run && !files.is_empty() {
         let mut user_input = String::new();
 
@@ -355,7 +360,7 @@ fn main() {
 
         if user_input.trim().to_lowercase() == "y" {
             for (src_file, dest_file) in files.into_iter() {
-                println!(
+                eprintln!(
                     "{}  {}  {}",
                     src_file.to_str().unwrap().green(),
                     "->".cyan(),
@@ -367,7 +372,7 @@ fn main() {
     }
 
     if files.is_empty() {
-        println!("No files found to copy. Exiting..");
+        eprintln!("No files found to copy. Exiting..");
         exit(0)
     }
 
@@ -380,16 +385,16 @@ fn main() {
             .unwrap()
             .progress_chars("#>-"),
     );
-    pb.enable_steady_tick(Duration::from_millis(50));
+    pb.enable_steady_tick(Duration::from_millis(100));
 
     for (src_file, dest_file) in files.into_iter() {
         pb.set_message(format!("{}", src_file.display()));
         pb.inc(1);
 
-        if let Err(err) = std::fs::create_dir_all(dest_file.parent().unwrap().unwrap().as_path()) {
-            println!(
-                "Error in creating directory: \"{}\". Skipping file: {} \nErr:{err}",
-                dest_file.parent().unwrap().unwrap().as_path().display(),
+        if let Err(err) = std::fs::create_dir_all(dest_file.parent()?.unwrap()) {
+            eprintln!(
+                "Error in creating directory: \"{:?}\". Skipping file: {} \nErr:{err}",
+                dest_file.parent()?.unwrap(),
                 src_file.display(),
             );
             files_failed.push(src_file);
@@ -399,16 +404,17 @@ fn main() {
         let status = process::Command::new(&adb_path)
             .arg("pull")
             .arg("-a")
-            .arg(src_file.as_path().as_unix_str().to_str().unwrap())
+            .arg(src_file.to_str().unwrap())
             .arg(dest_file.as_path().to_str().unwrap())
             .stdout(process::Stdio::null())
             .status()
-            .expect("Failed to start process to pull files using adb");
+            .context("Failed to start process to pull files using adb")?;
 
         if status.success() {
             files_done.push(src_file)
         } else {
-            files_failed.push(src_file)
+            eprintln!("Failed to copy: {:?}", &src_file);
+            files_failed.push(src_file);
         }
     }
 
@@ -416,14 +422,14 @@ fn main() {
 
     let success_path = PathBuf::from("./files_done.txt");
     let failed_path = PathBuf::from("./files_failed.txt");
-    println!(
+    eprintln!(
         "Done! Successfully copied {} files. Files written to {:?}",
         files_done.len(),
         success_path
     );
 
     if !files_failed.is_empty() {
-        println!("Failed to copy {} files. Failed files written to {:?}", files_failed.len(), failed_path);
+        eprintln!("Failed to copy {} files. Failed files written to {:?}", files_failed.len(), failed_path);
     }
 
     let mut file = OpenOptions::new().append(true).create(true).open(success_path.as_path()).unwrap();
@@ -443,4 +449,5 @@ fn main() {
             }
         }
     }
+    Ok(())
 }
